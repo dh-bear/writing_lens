9782641ebbf7a4ec1ec1bf94148e1c70d7452abf
dh-bear <daniel_hadar@brown.edu>
2023-07-28 12:58:43 -0700

    plane

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import './styles.css';
-
 export default function Page() {
   return (
     <div>
-      <h1>Welcome to My Page</h1>
+      <h1>WWWWWelcome to My Page</h1>
       <p>This is a simple example of a static page in a Next.js application.</p>
 
       <section>
@@ -15,7 +13,6 @@ export default function Page() {
         <h2>Another Section</h2>
         <p>Vestibulum id neque ut lacus laoreet interdum ut eget nibh. Nunc aliquam nulla non diam volutpat, in tristique mi convallis.</p>
       </section>
-
       <footer>
         <p>Thank you for visiting my page!</p>
       </footer>
